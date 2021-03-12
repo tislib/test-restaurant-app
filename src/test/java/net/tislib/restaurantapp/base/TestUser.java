6daa828540653a0317bfbe307a5fa9bd5a3a2164
Taleh Ibrahimli <talehsmail@gmail.com>
2021-03-13 03:42:52 +0400

    - fixes for build issues

--- src/test/java/net/tislib/restaurantapp/base/TestUser.java
+++ src/test/java/net/tislib/restaurantapp/base/TestUser.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum TestUser {
+    REGULAR_USER_1("user@testapp.com", "admin123"),
+    REGULAR_OWNER_1("owner@testapp.com", "admin123"),
+    REGULAR_OWNER_2("owner2@testapp.com", "admin123"),
     ADMIN_USER("admin@testapp.com", "admin123");
 
     private final String email;
--- src/test/java/net/tislib/restaurantapp/controller/RestaurantControllerIT.java
+++ src/test/java/net/tislib/restaurantapp/controller/RestaurantControllerIT.java
@@ -6,28 +6,54 @@ import net.tislib.restaurantapp.data.RestaurantResource;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import static net.tislib.restaurantapp.constant.ApiConstants.API_RESTAURANTS;
+import static net.tislib.restaurantapp.constant.ApiConstants.PATH_ID;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class RestaurantControllerIT extends BaseIntegrationTest {
 
-    public static final String STRING = "/api/1.0/restaurants";
     public static final String RESTAURANT_1 = "restaurant-1";
 
     @Test
     public void createRestaurantAndGet200Success() throws Exception {
-        auth(TestUser.ADMIN_USER);
+        auth(TestUser.REGULAR_OWNER_1);
 
         RestaurantResource resource = new RestaurantResource();
         resource.setName(RESTAURANT_1);
         resource.setOwner(currentUser);
 
-        mockMvc.perform(post(STRING).content(jsonContent(resource)))
+        mockMvc.perform(post(API_RESTAURANTS).content(jsonContent(resource)))
                 .andExpect(ResultMatcher.matchAll(
                         status().is2xxSuccessful()
                 ))
                 .andExpect(jsonPath("$.name").value(RESTAURANT_1));
+    }
+
+    @Test
+    public void createRestaurantByRegularUserAndGet403PermissionDenied() throws Exception {
+        auth(TestUser.REGULAR_USER_1);
+
+        RestaurantResource resource = new RestaurantResource();
+        resource.setName(RESTAURANT_1);
+        resource.setOwner(currentUser);
+
+        mockMvc.perform(post(API_RESTAURANTS).content(jsonContent(resource)))
+                .andExpect(ResultMatcher.matchAll(
+                        status().isForbidden()
+                ));
+    }
+
+    @Test
+    public void deleteRestaurantByOwnerUserAndGet403PermissionDenied() throws Exception {
+        auth(TestUser.REGULAR_OWNER_1);
+
+        mockMvc.perform(delete(API_RESTAURANTS + PATH_ID, 1))
+                .andExpect(ResultMatcher.matchAll(
+                        status().isForbidden()
+                ));
     }
 
 }
